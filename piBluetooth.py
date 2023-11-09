58b58e8076edcfd001ad0ecd2ea6d4cb5222a28f
adanahy5 <70297759+adanahy5@users.noreply.github.com>
2023-11-09 14:11:32 -0500

    added basic chording to bluetooth module on the py4 side

--- piBluetooth.py
+++ piBluetooth.py
@@ -1,13 +1,29 @@
 from bluepy import btle
 from binascii import hexlify
+import csv
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         super().__init__()
+        self.chordmap = self.init_chords()
 
     def handleNotification(self, cHandle, data):
         text = bytes.fromhex(hexlify(data).decode()).decode('utf-8')
         print("Received data: %s" % text)
+        if(text in self.chordmap) :
+            print(self.chordmap.get(text))
+        else:
+            print("Not a chord")
+
+    def init_chords(self):
+        with open('chords.csv', newline = '') as csvfile:
+            reader = csv.DictReader(csvfile)
+            keys = []
+            vals = []
+            for row in reader:
+                key.append(row["CHORD"])
+                key.append(row["WORD"])
+        return {keys[i]:vals[i] for i in range (len(keys))}
 
 pico_mac_address = 'D8:3A:DD:44:B2:84'
 characteristic_uuid = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
