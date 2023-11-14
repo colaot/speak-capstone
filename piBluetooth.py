a2031b098b1219e1466f2f4ef07c3ebbab70e4c2
Conner Ellis <57118235+deftcrab8@users.noreply.github.com>
2023-11-14 14:09:55 -0500

    added chord buffer

--- piBluetooth.py
+++ piBluetooth.py
@@ -5,16 +5,43 @@ import csv
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         super().__init__()
+        self.buffer = set() # Buffer to store keys
         self.chordmap = self.init_chords()
-        self.chord_buffer = []
+        self.chords = [] # Final chord array
 
     def handleNotification(self, cHandle, data):
         text = bytes.fromhex(hexlify(data).decode()).decode('utf-8')
         print("Received data: %s" % text)
+
+        key, direction, state = self.parse_data(text)
+
+        if state == 'Pressed':
+            self.buffer.add((key, direction))
+        elif state == 'Released':
+            if (key, direction) in self.buffer:
+                self.buffer.remove((key, direction))
+
+        # Check all keys are released
+        if not self.buffer:
+            self.chords.append(text)
+            print("Stored chord: ", text)
+            self.buffer.clear()
+
+
+    def parse_data(self, text):
+        try:
+            key, direction, state = text.strip('()').split(', ')
+            return key, direction, state
+        except ValueError:
+            print("Invalid data format")
+            return None, None, None
+
+''' commented out for later development
         if(text in self.chordmap) :
             print(self.chordmap.get(text))
         else:
             print("Not a chord")
+
 
     def init_chords(self):
         with open('chords.csv', newline = '') as csvfile:
@@ -26,6 +53,7 @@ class MyDelegate(btle.DefaultDelegate):
                 key.append(row["WORD"])
         return {keys[i]:vals[i] for i in range (len(keys))}
 
+'''
 
 pico_mac_address = 'D8:3A:DD:44:B2:84'
 characteristic_uuid = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
