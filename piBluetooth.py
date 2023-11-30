b53f6036f2777e4e4fb20cba02baa464449086e4
Andre Danahy <70297759+adanahy5@users.noreply.github.com>
2023-11-30 13:00:05 -0500

    Update piBluetooth.py
    Added module to convert keystrokes to chars

--- piBluetooth.py
+++ piBluetooth.py
@@ -9,26 +9,31 @@ class MyDelegate(btle.DefaultDelegate):
         self.chordmap = self.init_chords()
         self.chords = [] # Final chord array
 
+# Up, Right, Down, Left, Center :: 0, 1, 2, 3, 4
+# Pressed, Released :: 1, 0
+
     def handleNotification(self, cHandle, data):
         text = bytes.fromhex(hexlify(data).decode()).decode('utf-8')
         print("Received data: %s" % text)
 
         key, direction, state = self.parse_data(text)
 
-        if state == 'Pressed' and not self.check_buffer_for_key(key, direction):
+        if state == 1 and not self.check_buffer_for_key(key, direction):
             self.buffer.add((key, direction, state))
-        elif state == 'Released':
-            if (key, direction, 'Pressed') in self.buffer:
-                self.buffer.discard(key, direction, 'Pressed')
+        elif state == 0:
+            if (key, direction, 1) in self.buffer:
+                self.buffer.discard(key, direction, 1)
                 self.buffer.add(key, direction, state)
 
         # Check all keys are released
         if self.check_all_keys_released():
-            print("Clearing buffer . . .")
+
             print("Buffer contains:")
             print(self.buffer)
+            print(self.translate_to_chars())
             #above prints needs to be replaced with translate to chars
             #and then from that into chords
+            print("Clearing buffer . . .")
             self.buffer.clear()
 
     #checks if key (int) already exists in buffer
@@ -41,19 +46,67 @@ class MyDelegate(btle.DefaultDelegate):
             # case 3: key is same new is center, old is not: False
             # case 4: key is same both center: True
             # case 5: keys not the same: False
+
+            #THIS HURTS ABILITY TO SOLO CENTER PRESS, NEED FIX
             if buf_key == key:
-                if dir != 'Center' and buf_dir != 'Center':
+                if dir != 4 and buf_dir != 4:
                     return True
-                if dir == 'Center' and buf_dir == 'Center':
+                if dir == 4 and buf_dir == 4:
                     return True
         return False
 
     def check_all_keys_released(self):
         for tuple in self.buffer:
             (buf_key, buf_dir, buf_state) = tuple
-            if buf_state == 'Pressed':
+            if buf_state == 1:
                 return False
         return True
+
+    def translate_to_chars(self):
+        tuple_buf = []
+        char_buf = []
+        char_dict = self.init_char_map()
+        # check each key for a center and other press
+        for tuple in self.buffer:
+            (buf_key, buf_dir, buf_state) = tuple
+            if (buf_dir != 4):
+                # if keystroke is not center press, check for corresponding center press
+                if center_press_in_buf(buf_key):
+                    tuple_buf.append((buf_key, buf_dir, 1))
+                else:
+                    tuple_buf.append((buf_key, buf_dir, 0))
+            else:
+                # if keystroke is center press, check for corresponding non-center press
+                if not non_center_press_in_buf(buf_key):
+                    tuple_buf.append((buf_key, buf_dir, 0))
+        for tuple in tuple_buf:
+            char_buf.append(char_dict.get(tuple))
+        return char_buf
+
+    def center_press_in_buf(key):
+        for tuple in self.buffer():
+            (buf_key, buf_dir, buf_state) = tuple
+            if (buf_dir == 4 and buf_key == key)
+                return True
+        return False
+
+    def non_center_press_in_buf(key):
+            for tuple in self.buffer():
+                (buf_key, buf_dir, buf_state) = tuple
+                if (buf_dir != 4 and buf_key == key)
+                    return True
+            return False
+
+    def init_char_map(self):
+        with open('press_to_char.csv', newline = '') as csvfile:
+            reader = csv.DictReader(csvfile)
+            tuple = []
+            chars = []
+            for row in reader:
+                tuple.append((row["KEY"], row["DIRECTION"], row["CENTER"]))
+                chars.append(row["CHAR"])
+        return {tuple[i]:chars[i] for i in range(len(tuple))}
+
 
     def parse_data(self, text):
         try:
